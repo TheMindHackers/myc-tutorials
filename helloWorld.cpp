e5bf5e28f8ae5fced9532ba7b7fd3831bf7f7481
TheMindHackers <125470661+TheMindHackers@users.noreply.github.com>
2023-02-15 19:28:09 +0500

    Add files via upload
    my first tutorial

---
+++ helloWorld.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // basic intro of iostream
+    // basic intro of cout statements
+    // end of line (endl) and "\n"
+
+    cout<<"john wick 1\n\n\n"<<"hi mind hackers\n"<<"john wick 2";
+    //cout<<"\n";
+    //cout<<"hi mind hackers"<<endl;
+   
+    //cout<<"john wick 2";
+    return 0;
+}
