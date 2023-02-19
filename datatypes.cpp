3a55cdfee7fdfda1dc847e5bf43258a811ed04a9
TheMindHackers <125470661+TheMindHackers@users.noreply.github.com>
2023-02-20 00:05:53 +0500

    Add files via upload
    A basic overview of c++ datatypes?

---
+++ datatypes.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{   
+    //  datatype name =  initialization value;
+    // 2 integers add
+    float val1=0.213;
+    float val2= 1.22;
+    cout<<val1+val2;
+
+    return 0;
+}
