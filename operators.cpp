19bdf3a89cbd17981390b5f219737eabc85e7d99
TheMindHackers <125470661+TheMindHackers@users.noreply.github.com>
2023-03-11 00:30:44 +0500

    Add files via upload
    comparison and logical operators in c++

---
+++ operators.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{   //-------------------------COMPARISION OPERATORS
+    // always return a boolean value// true or false
+    // == equal
+    // != not equal
+    // >= greater than or equal to
+    // <=
+    // >
+    // <
+    // int shabana=3;
+    // int rukshana=3;
+            //1 represents true
+            //0 represents false
+
+    
+    //cout<<(shabana==rukshana);
+    //cout<<(shabana!=rukshana);
+    //cout<<(shabana<rukshana);
+
+//--------------------------- LOGICAL OPERATORS ------------------
+    // AND &&  
+    // stm1 && stm2
+
+    // OR  ||
+    // stmt1 || stmt2
+
+
+    int a=2;
+    int b=2;
+            // stmt1 && stmt2   
+   // cout<<(  (a==b) && (a<b)    );
+    //  cout<<(  (a>b) || (a<b)    );
+    
+    return 0;
+}
