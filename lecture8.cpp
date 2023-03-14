dec4457766e3970b8fefc7bdef4e88b971f64c39
TheMindHackers <125470661+TheMindHackers@users.noreply.github.com>
2023-03-15 03:11:05 +0500

    Add files via upload

---
+++ lecture8.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{   
+
+    // ---------------------- setw()
+    // cout<<setw(4)<<1234<<endl;
+    // cout<<setw(4)<<234<<endl;
+    // cout<<setw(4)<<34<<endl;
+    // cout<<setw(4)<<4<<endl;
+
+    ///--------------------- contants
+        // const float pi=3.142314324324;
+        // pi=10;
+        // cout<<i;
+
+    //---------------------precedence
+    // cout<<2*1+2<<endl;
+    // cout<<2*(1+2);          // BODMAS rule
+
+    cout<<2*(1*3+1)+2*2<<endl;
+    cout<<2*1*3+1+2*2;  
+
+    return 0;
+}
